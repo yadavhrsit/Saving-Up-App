c4a966df081c56453e98177b1482e3196eefcebf
Harshit Yadav <inbox.harshityadav@gmail.com>
2024-04-29 03:41:19 +0530

    Login Signup pages designed

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Signup from './Pages/Signup';
+import Login from "./Pages/Login";
+import Home from "./Pages/Home";
+import {Routes,Route} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" Component={Home}></Route>
+        <Route path="login" Component={Login}></Route>
+        <Route path="signup" Component={Signup}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ client/src/Pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      Home
+      <a href="/signup" className="text-blue-500">
+        Sign up here
+      </a>
+      <a href="/login" className="text-blue-500">
+        login up here
+      </a>
+    </div>
+  );
+}
+
+export default Home
